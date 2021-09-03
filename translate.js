8cee938ae59d29e0ce829c244e2ef9f5d0ea6ed8
PiotrD94 <piotr.danecki94@gmail.com>
2021-09-03 12:16:52 +0200

    update

--- translate.js
+++ translate.js
@@ -1,4 +1,14 @@
-console.log("hello script is working")
+var txtinput = document.querySelector("#inputarea-translate");
+var txtoutput = document.querySelector("#outputarea-translate");
 var button = document.querySelector(
-    ".button")
-    button.addEventListener("click", callback)
+    "#button-translate");
+
+console.log("hello script is working");
+button.addEventListener("click", clickTranslate)
+
+function clickTranslate() {
+    console.log("Clicked!");
+    console.log("input", txtinput.value);
+    txtoutput.innerText = txtinput.value;
+}
+
